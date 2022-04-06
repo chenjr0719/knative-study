43fb95646ceb5e172d23ca5e734952c98b355c4b
Jacob Chen <chenjr0719@gmail.com>
2022-04-06 16:03:50 +0800

    feat: Add trigger

---
+++ deployments/ce-trigger/delete.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do SOURCE="$(readlink "$SOURCE")"; done
+ROOT_DIR="$(cd -P "$(dirname "$SOURCE")/../.." && pwd)"
+
+kubectl -n default delete -f $ROOT_DIR/deployments/ce-trigger/ce-trigger.yaml
