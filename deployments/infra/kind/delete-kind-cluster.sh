e1aba02800ea69f7f5fe8e5d385b7b808aaa6f75
Jacob Chen <chenjr0719@gmail.com>
2022-04-06 15:47:33 +0800

    refactor: Reorganize

--- deployments/infra/kind/delete-kind-cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do SOURCE="$(readlink "$SOURCE")"; done
-ROOT_DIR="$(cd -P "$(dirname "$SOURCE")/.." && pwd)"
-
-kind delete cluster
